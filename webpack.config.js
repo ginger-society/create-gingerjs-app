be170a678756bd9e5dc707dcde8bb1c8f352ecfb
Ginger Society Admin <admin@gingersociety.org>
2024-06-03 19:18:12 +0530

    feat: added build commands

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    library: {
+      type: "commonjs2",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  externals: {
+    react: "react",
+    "react-dom": "react-dom",
+  },
+};
